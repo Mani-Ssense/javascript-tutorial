ac1afbe2f4819969769c71ee7ea8165c7e5ab786
Mani Ahmadi Arsham <moneyfx@gmail.com>
2018-07-25 08:23:41 -0400

    loops

---
+++ 0009_loops_and_etc.js
@@ -0,0 +1,49 @@
+let a = 1;
+while (a <= 3) {
+    console.log(a);
+    a++;
+}
+/*
+1
+2
+3
+*/
+let b = 1;
+do {
+    console.log(b);
+    b++
+} while (b <=3);
+/*
+1
+2
+3
+*/
+for (let i = 1; i <= 3; i++) {
+    console.log(i);
+}
+/*
+1
+2
+3
+*/
+for (let current = 18; ; current++) {
+    if (current % 7 == 0) {
+      console.log(current); // 21
+      break;
+    }
+}
+const str = 'cloudy';
+
+switch (str) {
+    case "rainy":
+      console.log("Remember to bring an umbrella.");
+      break;
+    case "sunny":
+      console.log("Dress lightly.");
+    case "cloudy":
+      console.log("Go outside."); // Go outside.
+      break;
+    default:
+      console.log("Unknown weather type!");
+      break;
+  }
