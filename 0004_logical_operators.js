e3b856aa25f713c1352912873e358507df271a70
Mani Ahmadi Arsham <moneyfx@gmail.com>
2018-07-25 00:01:37 -0400

    logical operators

---
+++ 0004_logical_operators.js
@@ -0,0 +1,6 @@
+console.log(true && false) // false
+console.log(true && true) // true
+console.log(false || true) // true
+console.log(false || false) // false
+console.log(true ? 1 : 2);// 1
+console.log(false ? 1 : 2);// 2
