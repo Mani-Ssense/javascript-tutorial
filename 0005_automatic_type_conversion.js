88e902f67a43da14f94672f0395d354cf7b97b0f
Mani Ahmadi Arsham <moneyfx@gmail.com>
2018-07-25 01:47:30 -0400

    automatic conversion

---
+++ 0005_automatic_type_conversion.js
@@ -0,0 +1,14 @@
+console.log(8 * NaN) // NaN
+console.log(8 * undefined) // NaN 
+console.log(8 * null) // 0
+console.log("5" - 1) // 4
+console.log("5" + 1) // 51
+console.log(1 + "5") // 15
+console.log(1 - "5") // -4
+console.log("five" * 2) // NaN
+console.log(false == 0) // true
+
+console.log(null == undefined); // true
+console.log(null === undefined); // false
+console.log(null == 0); // false
+console.log(null === 0); // false
