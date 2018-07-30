4e37019c758e5c6f6f7b49db53c17b6f413b9395
Mani Ahmadi Arsham <moneyfx@gmail.com>
2018-07-29 23:45:55 -0400

    use single quotes for strings

--- 0013_reverse_string.js
+++ 0013_reverse_string.js
@@ -1,5 +1,5 @@
 const reverseString = function(str) {
-    return str.split("").reverse().join('');
+    return str.split('').reverse().join('');
 };
 
 console.log(reverseString('hello')); // olleh
